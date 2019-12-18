94233a83782f10b0767c0ce9539c07ad8335095b
lly <123@qq.com>
2019-12-18 11:07:42 +0800

    this is test

--- mybatis/src/day2/demo1/Test.java
+++ mybatis/src/day2/demo1/Test.java
@@ -1,5 +1,6 @@
 package day2.demo1;
 
+import day1.entity.User;
 import day2.demo1.dao.IUserDao;
 import org.apache.ibatis.session.SqlSession;
 import util.Util;
@@ -8,7 +9,10 @@ public class Test {
     public static void main(String[] args) {
         SqlSession session= Util.getSession();
         IUserDao dao=session.getMapper(IUserDao.class);
-        System.out.println(dao.selectById(1));
+        User u=dao.selectById(1);
+        u.setName("猪");
+        dao.updateByParam(u);
+        System.out.println(dao.selectByChose(u));
 
 
     }
--- mybatis/src/day2/demo1/dao/IUserDao.java
+++ mybatis/src/day2/demo1/dao/IUserDao.java
@@ -1,18 +1,12 @@
 package day2.demo1.dao;
 
 import day1.entity.User;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 public interface IUserDao {
     User selectById(Integer id);
 
-    User selectByIdAndName(@Param("userid") Integer id, @Param("username") String name);
+    User selectByParam(User u);
+    User selectByChose(User u);
+    void updateByParam(User u);
 
-    User selectByUser(User u);
-    void insertUser(User u);
-    void deleteById(Integer id);
-    void updateByUserId(User u);
-    List<User> selectAllUser();
 }
